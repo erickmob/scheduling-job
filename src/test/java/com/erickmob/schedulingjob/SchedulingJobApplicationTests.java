94ec5125d707692b7b21f6b3935a7872ea613dd3
erickmob <erickmob@gmail.com>
2020-07-10 09:50:53 -0300

    criando boilerplate do projeto

---
+++ src/test/java/com/erickmob/schedulingjob/SchedulingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmob.schedulingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
