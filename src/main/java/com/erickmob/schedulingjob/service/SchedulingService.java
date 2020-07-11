ca13d10669d6fc3f0a28355cffe2317f9e2f6996
erickmob <erickmob@gmail.com>
2020-07-11 13:04:57 -0300

    added some date validation

--- src/main/java/com/erickmob/schedulingjob/service/SchedulingService.java
+++ src/main/java/com/erickmob/schedulingjob/service/SchedulingService.java
@@ -1,14 +1,46 @@
 package com.erickmob.schedulingjob.service;
 
+import com.erickmob.schedulingjob.model.Job;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
 public class SchedulingService {
 
-    public String sortJobsForScheduling(){
-        return "todo";
+    private static Duration maxDurationHour = Duration.ofHours(8);
+
+    private LocalDateTime executionTimeWindowStart = LocalDateTime.of(
+            2019,
+            Month.NOVEMBER,
+            10,
+            9,
+            0,
+            0);
+
+    private  LocalDateTime executionTimeWindowEnd = LocalDateTime.of(
+            2019,
+            Month.NOVEMBER,
+            11,
+            12,
+            0,
+            0);
+
+    public List<ArrayList> sortJobsForScheduling(List<Job> jobsList, LocalDateTime inicioJanelaDeExecucao, LocalDateTime fimJanelaDeExecucao) {
+
+        inicioJanelaDeExecucao = getValidDate(inicioJanelaDeExecucao, executionTimeWindowStart);
+        fimJanelaDeExecucao = getValidDate(fimJanelaDeExecucao, executionTimeWindowEnd);
+        return null;
+    }
+
+    LocalDateTime getValidDate(LocalDateTime receivedDate, LocalDateTime defaultDate) {
+        return receivedDate == null ? defaultDate : receivedDate;
     }
 
 }
--- src/test/java/com/erickmob/schedulingjob/service/SchedulingServiceTest.java
+++ src/test/java/com/erickmob/schedulingjob/service/SchedulingServiceTest.java
@@ -1,10 +1,15 @@
 package com.erickmob.schedulingjob.service;
 
+import com.erickmob.schedulingjob.model.Job;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,6 +19,25 @@ class SchedulingServiceTest {
     @Autowired
     private SchedulingService schedulingService;
 
+    private List<Job> jobsList;
+
+    private  LocalDateTime executionTimeWindowStart = LocalDateTime.of(
+            2019,
+            Month.NOVEMBER,
+            10,
+            9,
+            0,
+            0);
+
+    private  LocalDateTime executionTimeWindowEnd = LocalDateTime.of(
+            2019,
+            Month.NOVEMBER,
+            11,
+            12,
+            0,
+            0);
+
+
     @BeforeEach
     void setUp() {
     }
@@ -22,8 +46,35 @@ class SchedulingServiceTest {
     void tearDown() {
     }
 
+
     @Test
-    void initialTest(){
-        assertEquals(schedulingService.sortJobsForScheduling(), "todo");
+    void whenValidDate(){
+        //given
+        LocalDateTime date = LocalDateTime.of(
+                2019,
+                Month.NOVEMBER,
+                11,
+                12,
+                0,
+                0);
+
+        //when
+        LocalDateTime result = schedulingService.getValidDate(date, executionTimeWindowStart);
+
+        //then
+        assertEquals(date, result);
+    }
+
+    @Test
+    void whenInvalidDate(){
+
+        //given
+        LocalDateTime date = null;
+
+        //when
+        LocalDateTime result = schedulingService.getValidDate(date, executionTimeWindowStart);
+
+        //then
+        assertEquals(executionTimeWindowStart, result);
     }
 }
